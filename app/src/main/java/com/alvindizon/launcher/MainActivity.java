16ed69904a64651cd97bad466e98e1683a9e3555
Alvin Dizon <alvin.dizon91@gmail.com>
2019-06-30 13:49:25 +0800

    Move app list logic to separate fragment, add Navigation component lib
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/alvindizon/launcher/MainActivity.java
+++ app/src/main/java/com/alvindizon/launcher/MainActivity.java
@@ -1,85 +1,21 @@
 package com.alvindizon.launcher;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
 
-    private List<AppModel> appList = new ArrayList<>();
-
-    private PackageManager packageManager;
-    private AppListAdapter appListAdapter;
-    private RecyclerView recyclerView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        packageManager = getPackageManager();
 
-        Intent intent = new Intent(Intent.ACTION_MAIN, null);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        List<ResolveInfo> resInfos = packageManager.queryIntentActivities(intent, 0);
-        Collections.sort(resInfos, new ResolveInfo.DisplayNameComparator(packageManager));
-        addToAppList(resInfos);
-
-        appListAdapter = new AppListAdapter(this::launchApp);
-        recyclerView = findViewById(R.id.rv_nav);
-
-        appListAdapter.setAppList(appList);
-
-        DividerItemDecoration itemDecorator = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
-        itemDecorator.setDrawable(Objects.requireNonNull(ContextCompat.getDrawable(this, R.drawable.recycler_horizontal_bottom_border)));
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.addItemDecoration(itemDecorator);
-        recyclerView.setAdapter(appListAdapter);
-
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     }
 
-    private void addToAppList(List<ResolveInfo> resInfos) {
-
-        for(ResolveInfo resolveInfo : resInfos) {
-            String appLabel =(String) resolveInfo.loadLabel(packageManager);
-            if(appLabel.equals(getString(R.string.app_name))) {
-                continue;
-            }
-            AppModel appModel = new AppModel();
-            appModel.setPackageName(resolveInfo.activityInfo.packageName);
-            appModel.setAppLabel(appLabel);
-            appModel.setLauncherIcon(packageManager.getApplicationIcon(resolveInfo.activityInfo.applicationInfo));
-            appList.add(appModel);
-        }
-
-    }
-
-    private void launchApp(String packageName) {
-
-        try {
-            startActivity(packageManager.getLaunchIntentForPackage(packageName));
-        } catch (Exception e) {
-            Toast.makeText(
-                    MainActivity.this,
-                    String.format("Error: Couldn't launch app: %s", packageName),
-                    Toast.LENGTH_LONG
-            ).show();
-        }
-    }
 }
