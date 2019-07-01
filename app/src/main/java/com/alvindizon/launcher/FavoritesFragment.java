98d1b58f4e6380007fd4c89e80e7255166031384
Alvin Dizon <alvin.dizon91@gmail.com>
2019-07-01 09:00:16 +0800

    Add overflow/option menu for displaying list of available apps from favorites
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

--- app/src/main/java/com/alvindizon/launcher/FavoritesFragment.java
+++ app/src/main/java/com/alvindizon/launcher/FavoritesFragment.java
@@ -1,5 +1,6 @@
 package com.alvindizon.launcher;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -11,6 +12,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.alvindizon.launcher.databinding.FragmentFavoritesBinding;
 
@@ -27,6 +30,8 @@ public class FavoritesFragment extends Fragment {
     private SharedPreferences preferences;
     private List<AppModel> faveList = new ArrayList<>();
     private List<String> packageNameList = new ArrayList<>();
+    private NavController navController;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -44,12 +49,34 @@ public class FavoritesFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        navController = ((MainActivity) requireActivity()).getNavController();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.button.setOnClickListener((v -> addFavorites()));
+        binding.toolbar.inflateMenu(R.menu.menu_add_apps);
+        binding.toolbar.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.menu_disp_add_apps:
+                    navController.navigate(R.id.action_favorites_dest_to_app_list_dest);
+                    return true;
+                default:
+                    break;
+            }
+            return super.onOptionsItemSelected(item);
+        });
+    }
+
+    @Override
     public void onStart() {
         super.onStart();
-        binding.button.setOnClickListener((v -> addFavorites()));
     }
 
     private void addFavorites() {
-        preferences.edit().putString(FAVORITES_KEY, TextUtils.join(SEPARATOR, packageNameList)).commit();
+        navController.navigate(R.id.action_favorites_dest_to_app_list_dest);
     }
 }
