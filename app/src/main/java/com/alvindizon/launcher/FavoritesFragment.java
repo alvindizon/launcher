9843eb38f303ba43b33403cbfdbe7e92cf0393ef
Alvin Dizon <alvin.dizon91@gmail.com>
2019-07-01 16:22:12 +0800

    Initial code for saving favorite apps
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

--- app/src/main/java/com/alvindizon/launcher/FavoritesFragment.java
+++ app/src/main/java/com/alvindizon/launcher/FavoritesFragment.java
@@ -1,41 +1,58 @@
 package com.alvindizon.launcher;
 
-import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.alvindizon.launcher.databinding.FragmentFavoritesBinding;
+import com.squareup.moshi.JsonAdapter;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import static com.alvindizon.launcher.MainActivity.FAVE_LIST;
 
 public class FavoritesFragment extends Fragment {
-    public static final String FAVORITES_KEY = "key_favorites";
-    public static final String SEPARATOR = ",";
+    public static final String TAG = FavoritesFragment.class.getSimpleName();
 
     private PackageManager packageManager;
-    private AppListAdapter appListAdapter;
+    private FaveListAdapter faveListAdapter;
     FragmentFavoritesBinding binding;
-    private SharedPreferences preferences;
     private List<AppModel> faveList = new ArrayList<>();
-    private List<String> packageNameList = new ArrayList<>();
+    private List<String> favePackageNameList = new ArrayList<>();
     private NavController navController;
+    private SharedPreferences preferences;
+    JsonAdapter<List<String>> faveAppJsonAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
         binding = FragmentFavoritesBinding.inflate(inflater, container, false);
+
+        navController = ((MainActivity) requireActivity()).getNavController();
+        packageManager = requireActivity().getPackageManager();
+        preferences = ((MainActivity) requireActivity()).getCustomSharedPrefs();
+        faveAppJsonAdapter = ((MainActivity) requireActivity()).getFaveAppJsonAdapter();
+
+        loadFaveAppList();
 
         if(faveList.isEmpty()) {
             binding.button.setVisibility(View.VISIBLE);
@@ -45,17 +62,51 @@ public class FavoritesFragment extends Fragment {
             binding.frameFav.setVisibility(View.VISIBLE);
         }
 
+        faveListAdapter = new FaveListAdapter(this::launchApp);
+        faveListAdapter.setAppList(faveList);
+        DividerItemDecoration itemDecorator = new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL);
+        itemDecorator.setDrawable(Objects.requireNonNull(ContextCompat.getDrawable(requireContext(), R.drawable.recycler_horizontal_bottom_border)));
+        binding.rvNav.setLayoutManager(new LinearLayoutManager(requireContext()));
+        binding.rvNav.addItemDecoration(itemDecorator);
+        binding.rvNav.setAdapter(faveListAdapter);
+
         return binding.getRoot();
     }
 
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        navController = ((MainActivity) requireActivity()).getNavController();
+    private void loadFaveAppList() {
+        String faveAppListJsonString = preferences.getString(FAVE_LIST, "");
+        if (!TextUtils.isEmpty(faveAppListJsonString)) {
+            try {
+                favePackageNameList = faveAppJsonAdapter.fromJson(faveAppListJsonString);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // transform favePackageNameList to appmodels
+        if (favePackageNameList != null && !favePackageNameList.isEmpty()) {
+            for (String packageName : favePackageNameList) {
+                Log.d(TAG, packageName);
+                AppModel appModel = new AppModel();
+                ApplicationInfo appInfo;
+                try {
+                    appInfo = packageManager.getApplicationInfo(packageName, 0);
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                    return;
+                }
+
+                appModel.setPackageName(appInfo.packageName);
+                appModel.setLauncherIcon(packageManager.getApplicationIcon(appInfo));
+                appModel.setAppLabel(packageManager.getApplicationLabel(appInfo).toString());
+                faveList.add(appModel);
+            }
+        }
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onViewCreated");
         super.onViewCreated(view, savedInstanceState);
         binding.button.setOnClickListener((v -> addFavorites()));
         binding.toolbar.inflateMenu(R.menu.menu_add_apps);
@@ -71,12 +122,15 @@ public class FavoritesFragment extends Fragment {
         });
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
     private void addFavorites() {
         navController.navigate(R.id.action_favorites_dest_to_app_list_dest);
+    }
+
+    private void launchApp(String packageName) {
+        try {
+            startActivity(packageManager.getLaunchIntentForPackage(packageName));
+        } catch (Exception e) {
+            Toast.makeText(requireContext(), "Could not launch app \" " + packageName + "\"", Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/alvindizon/launcher/MainActivity.java
+++ app/src/main/java/com/alvindizon/launcher/MainActivity.java
@@ -1,14 +1,24 @@
 package com.alvindizon.launcher;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String FAVE_LIST = "key_fave_list";
 
     private NavController navController;
+    private SharedPreferences sharedPreferences;
+    JsonAdapter<List<String>> faveAppJsonAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +26,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        sharedPreferences = getSharedPreferences(getString(R.string.app_name), 0);
+        Moshi moshi = new Moshi.Builder().build();
+
+        faveAppJsonAdapter = moshi.adapter(Types.newParameterizedType(List.class, String.class));
     }
 
     public NavController getNavController() {
         return navController;
     }
 
+    public SharedPreferences getCustomSharedPrefs() {
+        return sharedPreferences;
+    }
+
+    public JsonAdapter<List<String>> getFaveAppJsonAdapter() {
+        return faveAppJsonAdapter;
+    }
 }
