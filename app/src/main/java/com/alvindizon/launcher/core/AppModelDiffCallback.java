b48e69c0af0a1d678bae1c3e6830e5e8d6f707ee
Alvin Dizon <alvin.dizon@ghl.com>
2019-08-08 15:37:58 +0800

    Fix doubling of fave app list items when returning from launched app
    - Previously, launching an app and pressing the backpress would take the
    user back to a fave app screen containing duplicate apps. Initially, I
    thought that using DiffUtils will fix this, turns out I was actually
    appending an already existing list with new items every time I loaded the saved app list from shared
    preferences, since I used a global AppModel List inside the
    FavoritesViewModel.
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

---
+++ app/src/main/java/com/alvindizon/launcher/core/AppModelDiffCallback.java
@@ -0,0 +1,40 @@
+package com.alvindizon.launcher.core;
+
+import androidx.recyclerview.widget.DiffUtil;
+
+import java.util.List;
+
+public class AppModelDiffCallback extends DiffUtil.Callback {
+
+    private List<AppModel> oldList;
+    private List<AppModel> newList;
+
+    public AppModelDiffCallback(List<AppModel> oldList, List<AppModel> newList) {
+        this.oldList = oldList;
+        this.newList = newList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldList.get(oldItemPosition).getPackageName().equals(newList.get(newItemPosition).getPackageName());
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        AppModel oldApp = oldList.get(oldItemPosition);
+        AppModel newApp = newList.get(newItemPosition);
+
+        return oldApp.getPackageName().equals(newApp.getPackageName()) &&
+                oldApp.getAppLabel().equals(newApp.getAppLabel());
+    }
+}
